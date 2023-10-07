f3b05a2f5e500540cc5466591a2dfa28e1c0a993
Serdal58 <sserdalyildirim58@gmail.com>
2023-10-07 23:12:39 +0300

    ..

--- Otel.MVC/Controllers/AdminController.cs
+++ Otel.MVC/Controllers/AdminController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Otel.MVC.Models.Entity;
+    
 
 namespace Otel.MVC.Controllers
 {
     public class AdminController : Controller
     {
+
         // GET: Admin
+        OtelMvcEntities otl = new OtelMvcEntities();
+         
         public ActionResult Index()
         {
+            var degerler = otl.Otel.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniOtel()
+        {
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult YeniOtel(OtelMvcEntities oten)
+        {
+            return View();
+
+
+
         }
     }
 }
